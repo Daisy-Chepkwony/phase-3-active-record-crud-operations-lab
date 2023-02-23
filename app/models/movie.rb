1f24bbebce2407b523575bca4799e46eceaa293d
+ <+>
2023-02-23 23:13:30 +0300

    completed work

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,38 @@
 class Movie < ActiveRecord::Base
+    def self.create_with_title(title)
+        Movie.create(title:title)
+    end
+    def self.first_movie
+        Movie.first
+    end
+    def self.last_movie
+        Movie.last
+    end
+    def self.movie_count
+        Movie.count
+    end
+    def self.find_movie_with_id(id)
+       Movie.find(id)
+    end
+    def self.find_movie_with_attributes(attributes)
+        Movie.find_by(attributes)
+    end
+    def self.find_movies_after_2002
+        movies = Movie.where("release_date > ?", "2002")
+    end
+    def update_with_attributes
+        movie = Movie.find(id)
+        movie.update(title: "New Title", release_date: "2022")
+    end
+    def self.update_all_titles(title)
+        Movie.update_all(title:title)
+    end
+    def self.delete_by_id
+        Movie.delete(id)
+    end
+    def self.delete_all_movies
+        Movie.delete_all
+    end
+
 
 end
